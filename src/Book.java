014237c6ebb29348508e5a2c90d707ff814d3748
igor-lima101 <igormaksym96@gmail.com>
2025-09-01 20:08:17 -0300

    feat: add methods, getters and setter...

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,47 @@
 public class Book {
+
+    private String isbn;
+    private String author;
+    private String title;
+    private int quantity;
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Book(String isbn, String author, String title, int quantity){
+        this.isbn = isbn;
+        this.author = author;
+        this.title = title;
+        this.quantity = quantity;
+    }
 }
